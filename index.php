a36704ac9eabe468c2cb6096e3f1265aacc44789
Abu Bakar <test@gmail.com>
2021-01-09 21:42:28 -0800

    Property web demo test

---
+++ index.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Page Title</title>
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div class="container-fluid bg-dark">
+		<div class="container">
+			<div class="row text-white">
+				<div class="col-lg-2">
+					<p class="pt-2 white-red-link">
+						<a href="#">
+							<i class="fas fa-phone-alt"></i> : 051-2741768
+						</a>
+					</p>
+				</div>
+				<div class="col-lg-7"></div>
+				<div class="col-lg-3 mx-auto">
+					<p class="pt-2 white-red-link">
+						<i class="fab mx-1 fa-facebook-f"></i>
+						<i class="fab mx-1 fa-twitter"></i>
+						<i class="fab mx-1 fa-instagram"></i> 
+						<i class="fab mx-1 fa-linkedin-in"></i> 
+						<a href="#" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">
+							Sign In
+							<i class="fa mx-1 fa-user"></i>
+						</a>
+					</p>
+					<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+						<div class="modal-dialog">
+							<div class="modal-content">
+								<div class="modal-header my-3">
+									<h3 class="modal-title text-dark">
+										Login & Registration
+									</h3>
+									<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+										<span aria-hidden="true" style="font-size: 30px;">&times;</span>
+									</button>
+								</div>
+								<div class="modal-body mt-5">
+									<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+										<li class="nav-item" role="presentation">
+											<a class="nav-link active mx-2 p-3 text-dark" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">
+												<strong> LOG IN </strong>
+											</a>
+										</li>
+										<li class="nav-item " role="presentation">
+											<a class="nav-link p-3 text-dark" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false"><strong>REGISTER</strong></a>
+										</li>
+									</ul>
+									<div class="tab-content" id="pills-tabContent">
+										<div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+											<form action="">
+												<div class="form-group">
+													<input type="email" class="form-control" placeholder="Email">
+												</div>
+												<div class="form-group">
+													<input type="password" class="form-control" placeholder="Password">
+												</div>
+												<button type="submit" class="btn btn-block buttons">LOG IN</button>
+											</form>
+										</div>
+										<div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
+											<form action="">
+												<div class="form-group">
+													<input type="text" class="form-control" placeholder="Username">
+												</div>
+												<div class="form-group">
+													<input type="number" class="form-control" placeholder="Contact no.">
+												</div>
+												<div class="form-group">
+													<input type="email" class="form-control" placeholder="Email">
+												</div>
+												<div class="form-group">
+													<input type="password" class="form-control" placeholder="Password">
+												</div>
+												<div class="form-group">
+													<input type="password" class="form-control" placeholder="Confirm Password">
+												</div>
+												<button type="submit" class="btn btn-block buttons">REGISTER</button>
+											</form>
+										</div> 
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>	
+		</div>
+	</div>
+	<div class="container-fluid bg-white" id="navbar">
+		<div class="container " >
+		<div class="row py-4">
+			<div class="col-lg-3">
+				<img src="images/logo.png" height="60px" alt="">
+			</div>
+			<div class="col-lg-6 mt-2 ">
+				<nav class="navbar navbar-expand-lg navbar-light">
+						<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+							<span class="navbar-toggler-icon"></span>
+						</button>
+						<div class="collapse navbar-collapse">
+							<div class="navbar-nav"	>
+								<a class="nav-link" href="#">Sell</a>
+								<a class="nav-link" href="#">Rent</a>
+								<a class="nav-link" href="#">Buy</a>
+								<a class="nav-link" href="#">Agents</a>
+								<a class="nav-link" href="#">Get A Quote</a>
+								<a class="nav-link" href="#">Invest</a>
+								<a class="nav-link" href="#">About Us</a>
+							</div>
+						</div>
+				</nav>
+			</div>
+			<div class="col-lg-3">
+				<div class="btn buttons px-3 mt-3">
+					<i class="fa fa-plus p-1 mx-2 bg-white text-danger rounded-circle"></i>
+					Add Property
+				</div>
+			</div>
+		</div>
+	</div>
+	</div>
+	<div class="container-fluid" id="bg-img">
+		<h2>
+			Property051.Com <br>
+			<small style="font-size: 70%;">Islamabad's Smartest Property Portal</small>
+		</h2>
+	</div>
+	<div class="container mt-5">
+		<div class="row">
+			<div class="col-lg-12 text-center mb-3">
+				<h3>
+					Our Projects
+				</h3>
+				Secure investments offering 20%+ annual return, 100% money-back guarantee! Call for details 051-2741768
+			</div>
+		</div>
+		<div class="row mt-3">
+			<div class="col-lg-3 p-1">
+				<div class="card">
+					<img class="card-img-top card-img-height" src="images/our-projects1.jpeg" alt="Card image cap">
+					<div class="card-body p-0">
+						<h6>AL-Falah Homes</h6>
+						<p>Islamabad</p>
+					</div>
+					<div class="card-footer p-0">
+						<p>
+							Starting From <span>PKR 6200/Sqr Feet</span>
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3 p-1">
+				<div class="card">
+					<img class="card-img-top card-img-height" src="images/our-projects2.jpg" alt="Card image cap">
+					<div class="card-body p-0">
+						<h6>Noon Village</h6>
+						<p>Islamabad</p>
+					</div>
+					<div class="card-footer p-0">
+						<p>
+							Starting From <span>PKR 6200/Sqr Feet</span>
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3 p-1">
+				<div class="card">
+					<img class="card-img-top card-img-height" src="images/our-projects3.jpg" alt="Card image cap">
+					<div class="card-body p-0">
+						<h6>LAVISH MALL</h6>
+						<p>Islamabad</p>
+					</div>
+					<div class="card-footer p-0">
+						<p>
+							Starting From <span>PKR 6200/Sqr Feet</span>
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3 p-1">
+				<div class="card">
+					<img class="card-img-top card-img-height" src="images/our-projects4.jpg" alt="Card image cap">
+					<div class="card-body p-0">
+						<h6>AXIS MALL</h6>
+						<p>Islamabad</p>
+					</div>
+					<div class="card-footer p-0">
+						<p>
+							Starting From <span>PKR 6200/Sqr Feet</span>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container my-5">
+		<div class="row">
+			<div class="col-lg-12 text-center">
+				<h3>
+					Feature Property
+				</h3>
+				Secure investments offering 20%+ annual return, 100% money-back guarantee! Call for details 051-2741768
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-12">
+				<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+					<div class="carousel-inner">
+						<div class="carousel-item active">
+							<img src="images/football-logo-background_1195-244.jpg" class="carousel-image">
+						</div>
+						<div class="carousel-item">
+							<img src="images/luxury-letter-e-logo-design_1017-8903.jpg" class="carousel-image">
+						</div>
+						<div class="carousel-item">
+							<img src="images/retro-label-on-rustic-background_82147503374.jpg" class="carousel-image">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script></body>
+	</html>
